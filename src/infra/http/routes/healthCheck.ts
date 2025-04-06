f6482a8dc34c46b64b7ecdf0c607b7215fefcbce
Efraim Andrade <efraim.dev@gmail.com>
2025-04-06 14:12:36 -0300

    remove app server step and add the ecs

--- src/infra/http/routes/healthCheck.ts
+++ src/infra/http/routes/healthCheck.ts
@@ -2,6 +2,6 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 
 export const healthCheckRoute: FastifyPluginAsyncZod = async server => {
   server.get('/health', async (_, reply) => {
-    await reply.status(200).send({ message: 'Nice! 👍' })
+    await reply.status(200).send({ message: 'Nice demais! 👍' })
   })
 }
